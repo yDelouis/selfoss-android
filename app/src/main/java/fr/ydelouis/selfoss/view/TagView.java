3615d7183c00fb30a3d9c443d05ef85bdac556b6
yDelouis <ydelouis@gmail.com>
2014-03-24 13:49:17 +0100

    Add counter for tags

--- app/src/main/java/fr/ydelouis/selfoss/view/TagView.java
+++ app/src/main/java/fr/ydelouis/selfoss/view/TagView.java
@@ -3,7 +3,7 @@ package fr.ydelouis.selfoss.view;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.androidannotations.annotations.EViewGroup;
@@ -13,12 +13,13 @@ import fr.ydelouis.selfoss.R;
 import fr.ydelouis.selfoss.entity.Tag;
 
 @EViewGroup(R.layout.view_tag)
-public class TagView extends LinearLayout {
+public class TagView extends RelativeLayout {
 
 	private Tag tag;
 
 	@ViewById protected View color;
 	@ViewById protected TextView name;
+	@ViewById protected TextView count;
 
 	public TagView(Context context) {
 		super(context);
@@ -36,6 +37,8 @@ public class TagView extends LinearLayout {
 		this.tag = tag;
 		color.setBackgroundColor(tag.getColor());
 		name.setText(tag.getName(getContext()));
+		count.setText(String.valueOf(tag.getUnread()));
+		count.setVisibility(tag.getUnread() == 0 ? View.GONE : View.VISIBLE);
 	}
 
 	public Tag getTag() {
